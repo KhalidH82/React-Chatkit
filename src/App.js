96a9cc1e12d1b77ce7e515fff8d6df0da4d59c2a
Khalid Hosein <khalid.hosein9@gmail.com>
2018-06-28 14:41:00 -0400

    Setup connection to chatkit API and ablew to recieve messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import { tokenUrl, instanceLocator } from './config'
+
 class App extends React.Component {
+
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+
+            instanceLocator,
+            userId: "Khosein",
+            tokenProvider: new Chatkit.TokenProvider({
+                    url: tokenUrl
+            })
+
+        })
+
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: 10484500,
+                hooks: {
+                    onNewMessage: message => {
+                        console.log('message.text: ', message.text)
+                    }
+                }
+            })
+        })
+    }
+
     render() {
         return (
             <div className="app">
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/59bbce25-a07e-411f-b524-914666e3a79e/token";
+const instanceLocator = "v1:us1:59bbce25-a07e-411f-b524-914666e3a79e";
+
+export { tokenUrl, instanceLocator }
